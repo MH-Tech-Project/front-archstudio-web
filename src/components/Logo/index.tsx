95ee3e0f41c5f7752655eb6da50dd4f6469ea519
codemathsz <mo284891@gmail.com>
2025-09-23 16:38:10 -0300

    feat: Header adjusts

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
     return(
         <div className="flex items-center gap-2">
             <img src="/logo.svg" alt="" />
-            <h1 className="text-xl font-bold text-foreground">ArchStudio Pro</h1>
+            <h1 className="text-xl font-bold text-[#E8EAEE]">ArchStudio Pro</h1>
         </div>
     )
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,14 +3,14 @@ import ThemeToggle from "../ThemeToggle";
 
 export default function Header() {
     return(
-        <header className="w-full flex justify-around items-center mb-8 gap-52 py-4 bg-card/60 backdrop-blur-lg border-b border-gray-900">
+        <header className="w-full flex justify-around items-center mb-8 gap-52 py-4 bg-card/60 backdrop-blur-lg border-b border-[#34373D] fixed top-0 z-999">
             <Logo />
             <div className="flex items-center gap-16">
-                <nav className="hidden md:flex items-center gap-8 text-foreground">
-                    <a href="#" className="hover:underline text-base font-medium hover:text-primary transition-colors">Planos</a>
-                    <a href="#" className="hover:underline text-base font-medium hover:text-primary transition-colors">Sobre Nós</a>
-                    <a href="#" className="hover:underline text-base font-medium hover:text-primary transition-colors">Ajuda</a>
-                    <button className="bg-background text-foreground py-2 px-4 rounded-xl border border-border hover:bg-muted transition-colors">Login</button>
+                <nav className="hidden md:flex items-center gap-8 text-[#9FA3AD]">
+                    <a href="#Plan" className="hover:underline text-base font-medium hover:text-[#E8EAEE] transition-colors">Planos</a>
+                    <a href="#About" className="hover:underline text-base font-medium hover:text-[#E8EAEE] transition-colors">Sobre Nós</a>
+                    <a href="#Help" className="hover:underline text-base font-medium hover:text-[#E8EAEE] transition-colors">Ajuda</a>
+                    <button className="bg-black text-foreground py-2 px-4 rounded-xl border border-black hover:bg-muted hover:text-[#E8EAEE] cursor-pointer">Login</button>
                 </nav>
                 <ThemeToggle />
             </div>
