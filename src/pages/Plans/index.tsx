15a57128e36c3d10ca535cfdd57fbc8664db7ec2
codemathsz <mo284891@gmail.com>
2025-09-25 16:19:01 -0300

    feat: screen plans

---
+++ src/pages/Plans/index.tsx
@@ -0,0 +1,20 @@
+import Header from "../../components/Header";
+import PlansSelector from "../../components/PlansSelector";
+
+export default function Plans() {
+    return(
+        <div className="w-full min-h-screen flex flex-col justify-center items-center">
+            <Header />
+            <div className="w-full flex flex-col gap-16 justify-center items-center mt-40 lg:mt-20">
+                <div className="w-11/12 md:w-3/5 flex flex-col justify-center items-center text-center gap-4">
+                    <h1 className="text-3xl md:text-5xl text-foreground font-bold flex-wrap text-center">Escolha o plano ideal para você</h1>
+                    <p className="w-4/5  lg:w-3/5 text-base md:text-xl text-[#9FA3AD] font-light text-center">Soluções completas para arquitetos e urbanistas, desde projetos individuais até grandes escritórios</p>
+                </div>
+
+                <div className="w-4/5">
+                    <PlansSelector />
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Plans from './pages/Plans'
 
 export const router = createBrowserRouter([
   {
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
   {
     path: '/login',
     element: <Login />,
+  },
+  {
+    path: '/plans',
+    element: <Plans />,
   }
 ])
