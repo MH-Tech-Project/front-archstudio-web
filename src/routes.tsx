1b5294688e3f1ac526091c863942450bf129411b
codemathsz <mo284891@gmail.com>
2025-09-26 13:05:56 -0300

    feat: new route signup

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Plans from './pages/Plans'
+import Signup from './pages/Signup'
 
 export const router = createBrowserRouter([
   {
@@ -15,5 +16,9 @@ export const router = createBrowserRouter([
   {
     path: '/plans',
     element: <Plans />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
   }
 ])
