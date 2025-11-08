d66970f4aca478a9ee60ab660ea134287e57cc6a
codemathsz <mo284891@gmail.com>
2025-11-08 13:16:19 -0300

    feat: use type navigate hook

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,14 +1,16 @@
-import { useNavigate } from "react-router-dom";
+import type { RedirectTo } from "../../routes";
+import { useTypedNavigate } from "../../hooks/useTypedNavigation";
 
 interface LogoProps {
     fixedColor?: boolean
+    redirectTo: RedirectTo
 }
 
-export default function Logo({fixedColor}: LogoProps) {
-    const navigate = useNavigate();
+export default function Logo({fixedColor, redirectTo}: LogoProps) {
+     const { navigateTo } = useTypedNavigate();
 
     return(
-        <div className="flex items-center gap-2 cursor-pointer" onClick={() => navigate('/')}>
+        <div className="flex items-center gap-2 cursor-pointer" onClick={() => navigateTo(redirectTo)}>
             <img src="/logo.svg" alt="" />
             <h1 className="text-xl font-bold text-foreground" style={{color: fixedColor ? '#E8EAEE' : undefined}}>ArchStudio Pro</h1>
         </div>
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,18 +1,19 @@
-import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 import { useAuth } from "../../hooks/useAuth";
+import { useTypedNavigate } from "../../hooks/useTypedNavigation";
+import { routes } from "../../routes";
 
 export default function Dashboard() {
     const { logout } = useAuth();
-    const navigate = useNavigate();
+    const { navigateTo } = useTypedNavigate()
 
     const handleLogout = () => {
         logout();
-        navigate('/');
+        navigateTo(routes.home);
     }
 
     return(
-        <div className="flex flex-col justify-center items-center h-screen w-full gap-8"> 
+        <div className="flex-1 flex flex-col justify-center items-center w-full gap-8"> 
             <p>Dashboard</p>
             <Button variant="primary" size="lg" onClick={() => handleLogout()}>
                 <p>Logout</p>
